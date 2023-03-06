bf032a75689b598b550c4901dfd64358234603e9
Dmytro Dmytrenko <dmytro.dmytrenko@dxc.com>
2023-03-06 15:12:44 +0200

    scripts: update delete for sui

--- bash/Delete-Sui.sh
+++ bash/Delete-Sui.sh
@@ -4,7 +4,6 @@ function delete() {
 cd $SDD_NM_HOME/.Sui/sui
 docker-compose down -v
 sudo rm -rf $SDD_NM_HOME/.Sui/sui /var/sui/ /usr/local/bin/sui*
-sudo rm /etc/systemd/system/suid.service
 }
 
 delete
