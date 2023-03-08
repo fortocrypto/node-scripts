7656b31e384d8fb2ad3c0de2dfd27e7f9ffd666a
Dmytro Dmytrenko <dmytro.dmytrenko@dxc.com>
2023-03-08 10:34:01 +0200

    meson: Install script updated

--- bash/Install-Meson.sh
+++ bash/Install-Meson.sh
@@ -10,12 +10,12 @@ cd
 sudo ufw disable
 sudo ufw allow 443
 cd $SDD_NM_HOME/.Meson
-wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.19/meson_cdn-linux-amd64.tar.gz' && tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz && cd ./meson_cdn-linux-amd64 && sudo ./meson_cdn-linux-amd64/service install meson_cdn
-sudo ./meson_cdn-linux-amd64/meson_cdn config set --token=$token --https_port=443 --cache.size=30
+wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.19/meson_cdn-linux-amd64.tar.gz' && tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz && cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
+sudo ./meson_cdn config set --token=$token --https_port=443 --cache.size=30
 echo 'export MESON=$SDD_NM_HOME/.Meson/meson_cdn-linux-amd64' >>~/.bash_profile
 source ~/.bash_profile
-sudo ./meson_cdn-linux-amd64/service start meson_cdn
-sudo ./meson_cdn-linux-amd64/service status meson_cdn
+sudo ./service start meson_cdn
+sudo ./service status meson_cdn
 }
 
 install
