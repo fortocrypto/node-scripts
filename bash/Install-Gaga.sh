79cd31ffa0a48d5257e9aab0624e5d888f289d71
Dmytro Dmytrenko <dmytro.dmytrenko@dxc.com>
2023-03-10 00:47:23 +0200

    scripts: add sleep to gaga install

--- bash/Install-Gaga.sh
+++ bash/Install-Gaga.sh
@@ -8,10 +8,11 @@ sudo apt-get install -f -y
 sudo apt install tar curl git ufw ca-certificates -y
 cd $SDD_NM_HOME/.Gaga
 curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/22/app/1.0.3/app-1_0_3.tar.gz && tar -zxf app-linux-amd64.tar.gz && rm -f app-linux-amd64.tar.gz && cd ./app-linux-amd64 && sudo ./app service install
+sleep 3
 sudo ./app service start
-./app status
+sleep 30
 sudo ./apps/gaganode/gaganode config set --token="$yourtoken"
-./app restart
+sudo ./app restart
 }
 
 install
