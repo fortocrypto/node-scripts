a5a6068505f9843613d5688ccc3c1f0de5a1030f
Dmytro Dmytrenko <dmytro.dmytrenko@dxc.com>
2023-03-17 01:49:34 +0200

    scripts: nolus with custom ports

--- bash/Install-Nolus.sh
+++ bash/Install-Nolus.sh
@@ -59,6 +59,20 @@ sudo systemctl restart systemd-journald
 sudo systemctl daemon-reload
 sudo systemctl enable nolusd
 sudo systemctl restart nolusd
+sleep 5
+available_ports=$(ss -Htan | awk '{print $4}' | cut -d ':' -f 2 | grep -v -E '^(0|1|5|6|7|8|9)' | sort -un)
+selected_ports=$(echo "$available_ports" | shuf -n 8)
+i=1
+for port in $selected_ports; do
+varname="var$i"
+eval $varname=$port
+((i++))
+done
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$var1\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$var2\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$var3\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$var4\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$var5\"%" $HOME/.nolus/config/config.toml
+sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$var6\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$var7\"%" $HOME/.nolus/config/app.toml
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$var2\"%" $HOME/.nolus/config/client.toml
+sleep 3
+sudo systemctl restart nolusd
 }
 
 install
