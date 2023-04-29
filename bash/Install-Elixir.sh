1adb5924934c1650be2139b92ae72d5e9ee3046d
Dmytro Dmytrenko <dmytro.dmytrenko@dxc.com>
2023-04-30 00:02:32 +0300

    meta: fix one more time elixir

--- bash/Install-Elixir.sh
+++ bash/Install-Elixir.sh
@@ -13,7 +13,7 @@ sed -i.bak -e "s|^ENV ADDRESS *=.*|ENV ADDRESS=$adress|" $SDD_NM_HOME/.Elixir/Do
 sed -i.bak -e "s|^ENV PRIVATE_KEY *=.*|ENV PRIVATE_KEY=$private_key|" $SDD_NM_HOME/.Elixir/Dockerfile
 sed -i.bak -e "s|^ENV VALIDATOR_NAME *=.*|ENV VALIDATOR_NAME=$validator_name|" $SDD_NM_HOME/.Elixir/Dockerfile
 docker build . -f Dockerfile -t elixir-validator
-docker run -it --name ev elixir-validator
+docker run -dit --name ev elixir-validator
 }
 
 install
