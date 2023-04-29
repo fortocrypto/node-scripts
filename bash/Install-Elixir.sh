0248b78446557c8e97e8645d59ab3f289865aba9
Dmytro Dmytrenko <dmytro.dmytrenko@dxc.com>
2023-04-29 23:52:44 +0300

    bash: fix elixir

--- bash/Install-Elixir.sh
+++ bash/Install-Elixir.sh
@@ -9,9 +9,9 @@ sudo apt install curl git apt-transport-https ca-certificates software-propertie
 curl -s https://raw.githubusercontent.com/fortocrypto/node-scripts/master/bash/Install-Docker.sh | bash
 cd $SDD_NM_HOME/.Elixir
 wget https://files.elixir.finance/Dockerfile
-sed -i.bak -e "s|^ENV ADDRESS *=.*|ADDRESS=$adress|" $SDD_NM_HOME/.Elixir/Dockerfile
-sed -i.bak -e "s|^ENV PRIVATE_KEY *=.*|PRIVATE_KEY=$private_key|" $SDD_NM_HOME/.Elixir/Dockerfile
-sed -i.bak -e "s|^ENV VALIDATOR_NAME *=.*|VALIDATOR_NAME=$validator_name|" $SDD_NM_HOME/.Elixir/Dockerfile
+sed -i.bak -e "s|^ENV ADDRESS *=.*|ENV ADDRESS=$adress|" $SDD_NM_HOME/.Elixir/Dockerfile
+sed -i.bak -e "s|^ENV PRIVATE_KEY *=.*|ENV PRIVATE_KEY=$private_key|" $SDD_NM_HOME/.Elixir/Dockerfile
+sed -i.bak -e "s|^ENV VALIDATOR_NAME *=.*|ENV VALIDATOR_NAME=$validator_name|" $SDD_NM_HOME/.Elixir/Dockerfile
 docker build . -f Dockerfile -t elixir-validator
 docker run -it --name ev elixir-validator
 }
