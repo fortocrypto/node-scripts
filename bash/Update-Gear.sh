e763ba0e56ee0668c13bb8555e57423ac9e8b4cd
Dmytro Dmytrenko <dmytro.dmytrenko@dxc.com>
2023-05-15 23:42:59 +0300

    bash: new update gear node

--- bash/Update-Gear.sh
+++ bash/Update-Gear.sh
@@ -3,6 +3,7 @@
 function update() {
 sudo apt update && sudo apt upgrade -y
 sudo systemctl stop gear-node
+rm -rf /tmp/substrate-wasmer-cache/
 cd $SDD_NM_HOME/.Gear
 wget --no-check-certificate https://get.gear.rs/gear-nightly-linux-x86_64.tar.xz && \
 tar xvf gear-nightly-linux-x86_64.tar.xz && \
