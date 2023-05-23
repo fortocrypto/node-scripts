3c2d467721ea6c2afd9e3543a0a4c74ae57176cd
Dmytro Dmytrenko <dmytro.dmytrenko@dxc.com>
2023-05-23 23:49:47 +0300

    bash: update gaga install

--- bash/Install-Gaga.sh
+++ bash/Install-Gaga.sh
@@ -7,12 +7,12 @@ sudo dpkg --configure -a
 sudo apt-get install -f -y
 sudo apt install tar curl git ufw ca-certificates -y
 cd $SDD_NM_HOME/.Gaga
-curl -o app-linux-amd64.tar.gz  https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf app-linux-amd64.tar.gz && rm -f app-linux-amd64.tar.gz && cd ./app-linux-amd64 && sudo ./app service install
+curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64 && sudo ./apphub service install
 sleep 3
-sudo ./app service start
+sudo ./apphub service start
 sleep 30
-sudo ./apps/gaganode/gaganode config set --token="$yourtoken"
-sudo ./app restart
+sudo ./apps/gaganode/gaganode config set --token=`$yourtoken`
+./apphub restart
 }
 
 install
